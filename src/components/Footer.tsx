4338e0ad34e1e0470838415e31cf42f37be828ea
Shari <sun.shari09@gmail.com>
2023-08-03 21:41:23 -0400

    Add creds

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 const Footer = () => {
   return (
     <div className="text-gray-300 text-sm mr-auto h-16 mt-32">
       <hr className="border-gray-400" />
-      <p className="mt-2">© UTM ACE 2023 | Website designed and engineered by Shari Sun</p>
-      
+      <p className="mt-2">
+        © UTM ACE 2023 | Website designed and engineered by{' '}
+        <Link href="https://www.linkedin.com/in/shari-sun/" className="text-blue-600" target="_blank">Shari Sun</Link>
+      </p>
     </div>
   )
 }

a83058198ab547270412ee4088bdc1b556b7e8ae
Shari <sun.shari09@gmail.com>
2023-08-03 22:17:54 -0400

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+      <link rel="icon" href="./favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
